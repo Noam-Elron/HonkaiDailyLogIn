825905536db8aeff42909d93fefabf16fab9d72b
Noam-Elron <noam.elron.college@gmail.com>
2023-11-02 03:32:55 -0400

    removed get/set and useless variables + error handling for bad cookie format  input

--- star_rail_gateway.js
+++ star_rail_gateway.js
@@ -1,55 +1,29 @@
 class StarRailer 
 {
-    #request_options = {method : null, muteHttpExceptions : true, contentType : 'application/json;charset=UTF-8', headers : {Accept : 'application/json, text/plain, */*', Cookie : cookie, Origin : 'https://act.hoyolab.com', Referer : 'https://act.hoyolab.com/', 'User-Agent' : this.user_agent}, payload : JSON.stringify(this.payload)};
-    constructor(cookies, account_id, debug = false) 
+    static baseURL = 'https://sg-public-api.hoyolab.com/event/luna/os/';
+    // request_options is a private variable in order to not be messed with outside
+    #request_options = {method : null, muteHttpExceptions : true, contentType : 'application/json;charset=UTF-8', headers : {Accept : 'application/json, text/plain, */*', Cookie : cookie, Origin : 'https://act.hoyolab.com', Referer : 'https://act.hoyolab.com/', 'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'}, payload : JSON.stringify({"act_id": `${account_id}`, "lang": "en-us"})};
+    constructor(cookies, account_id) 
     {
-        this.baseURL = 'https://sg-public-api.hoyolab.com/event/luna/os/';
         this.cookies = this.parse_cookies(cookies);
-        this.user_agent =  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
-        this.payload = {"act_id": `${account_id}`, "lang": "en-us"};
     };
     
     async main() 
     {
+        // There has to be atleast one cookie, so in order to not repeat this request twice(because this is data that is relevent to a current month, hence shared by all users)
+        const monthly_rewards = this.fetch_info('home', this.cookies[0]);
         for (let i = 0; i < this.cookies.length; i++) 
         {
-            const account_info = await this.account_info(this.cookies[i])
+            const account_info = await this.#fetch_info('info', this.cookies[i]);
+             
 
         };
     }
 
-    async login(cookie) 
+    async #fetch_info(url_add_on, cookie) 
     {
-        
-        return fetch(this.baseURL + 'sign', options)
-    };
-
-    async fetch_info(url_add_on, cookie) 
-    {
-
-        const data = fetch(this.baseURL + url_add_on + this.account_id, options).getAllHeaders();
-        return data;
-    }
-
-    async monthly_rewards_list(cookie)
-    {
-        const account_info = await this.fetch_info('home?lang=en-us&act_id=e202303301540311');
-        const data = account_info.data;
-        return data;
-    }
-
-    get options() 
-    {
-        return this.#request_options;
-    }
-
-    set options(method) 
-    {
-        if (method !== "POST" || method !== "GET")
-        {
-            throw new Error("Current integrated methods are only POST or GET");
-        }
-        this.#request_options.method = method;
+        this.#request_options.headers.Cookie = cookie
+        return fetch(this.baseURL + url_add_on + '?' + this.account_id, options).getAllHeaders().data;
     }
 
     static fetch(url, options)
@@ -68,7 +42,11 @@ class StarRailer
     static parse_cookies(cookies) 
     // Seperated multiple accounts in case < 1 is inputted. Even if only one it leaves it in the same format so no change neccesary
     {
-        return cookies.split("#");
+        if (cookies)
+        {
+            return cookies.split("#");
+        }
+        throw new Error("Inputted Cookies Format Incorrect")
     };
 }
 
