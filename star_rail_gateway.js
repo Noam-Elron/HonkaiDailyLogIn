b9210f02546d082f9cb5d78d07f07fe0bf861226
Noam-Elron <noam.elron.college@gmail.com>
2023-11-02 02:39:27 -0400

    options parameter is now private and accessable by a getter+setter+beginning of unit testing

--- star_rail_gateway.js
+++ star_rail_gateway.js
@@ -1,81 +1,72 @@
 class StarRailer 
 {
-    constructor(cookies, account_id) 
+    #request_options = {method : null, muteHttpExceptions : true, contentType : 'application/json;charset=UTF-8', headers : {Accept : 'application/json, text/plain, */*', Cookie : cookie, Origin : 'https://act.hoyolab.com', Referer : 'https://act.hoyolab.com/', 'User-Agent' : this.user_agent}, payload : JSON.stringify(this.payload)};
+    constructor(cookies, account_id, debug = false) 
     {
-        this.baseURL = 'https://sg-public-api.hoyolab.com/event/luna/os/sign';
+        this.baseURL = 'https://sg-public-api.hoyolab.com/event/luna/os/';
         this.cookies = this.parse_cookies(cookies);
         this.user_agent =  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
         this.payload = {"act_id": `${account_id}`, "lang": "en-us"};
     };
-
-    async login() 
-    {
-        let header = 
-        {
-            Accept : 'application/json, text/plain, */*',
-            Cookie : this.cookies,
-            Origin : 'https://act.hoyolab.com',
-            Referer : 'https://act.hoyolab.com/',
-            'User-Agent' : this.user_agent
-        };
-
-        let options = 
-        {
-            method : 'POST',
-            muteHttpExceptions : true,
-            contentType : 'application/json;charset=UTF-8',
-            headers : header,
-            payload : JSON.stringify(this.payload)
-        };
     
+    async main() 
+    {
         for (let i = 0; i < this.cookies.length; i++) 
         {
-            options.headers.Cookie = this.cookies[i];
-            let resp = UrlFetchApp.fetch(this.baseURL, options);
-            let resp_code = resp.getResponseCode();
+            const account_info = await this.account_info(this.cookies[i])
 
-            if (resp_code === 200) 
-            {
-                continue
-            };
-            throw new Error("HTTP Error: " + resp_code);
         };
-        return true;
+    }
+
+    async login(cookie) 
+    {
+        
+        return fetch(this.baseURL + 'sign', options)
     };
 
-    /// When recieving user cookie input, if theres more than one user it has to be seperated
-    /// by a '#' to be able to run the login function on multiple accounts.
-    parse_cookies(cookies) 
+    async fetch_info(url_add_on, cookie) 
     {
-            return cookies.split("#");
-    };
 
-    get_info() 
+        const data = fetch(this.baseURL + url_add_on + this.account_id, options).getAllHeaders();
+        return data;
+    }
+
+    async monthly_rewards_list(cookie)
     {
-    let header = 
+        return await this.fetch_info('home?lang=en-us&act_id=e202303301540311')
+        const data = account_info.data;
+        return data;
+    }
+    static fetch(url, options)
+    {
+        const resp = UrlFetchApp.fetch(url, options);
+        const resp_code = resp.getResponseCode();
+
+        if (resp_code === 200) 
         {
-            'User-Agent' : this.user_agent,
-            Cookie : this.cookies
+            return resp;
         };
-
-        let options = 
-        {
-            method : 'GET',
-            muteHttpExceptions : true,
-            contentType : 'application/json, text/plain, */*',
-            headers : header,
-        };
-    const account_info = UrlFetchApp.fetch(this.baseURL + `/info?lang=en-us&act_id=${this.account_id}`, options);
-    let resp_code = resp.getResponseCode();
-
-    if (resp_code !== 200) 
-    {
         throw new Error("HTTP Error: " + resp_code);
+    }
+
+    static parse_cookies(cookies) 
+    /// Seperated multiple accounts in case < 1 is inputted. Even if only one it leaves it in the same format so no change neccesary
+    {
+        return cookies.split("#");
     };
 
+    get options() 
+    {
+        return this.#request_options;
+    }
 
-    const data = account_info.getAllHeaders().data;
-    
+    set options(method) 
+    {
+        if (method !== "POST" || method !== "GET")
+        {
+            throw new Error("Current integrated methods are only POST or GET");
+        }
+        this.#request_options.method = method;
     }
 }
 
---
+++ tests.js
@@ -0,0 +1,15 @@
+import { StarRailer } from "./star_rail_gateway";
+
+async function assert_req_settings(user) 
+{
+    if (typeof(cookies) !== "string")
+    {
+        throw new Error("Invalid Cookies Input")
+    } 
+}
+
+async function main() 
+{
+    let user = StarRailer(cookies, user_id);
+```
+}
